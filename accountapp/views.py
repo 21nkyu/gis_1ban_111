6a14bd83d946baf67561a5b00e0de90a20be6d57
21nkyu <adele7178@naver.com>
2021-07-12 16:22:36 +0900

    2021 07 12 004 1ban class commit 016

--- accountapp/views.py
+++ accountapp/views.py
@@ -46,4 +46,4 @@ class AccountCreateView(CreateView):        #createview 상속
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world') #class에서는 reverse_lazy를 사용한다. (reverse와 같다)
-    template_name = 'aountapp/create.html'
+    template_name = 'accountapp/create.html'
