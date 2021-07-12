79ee26a50dbf7cd40a2a8864700a48c440208cdc
21nkyu <adele7178@naver.com>
2021-07-12 16:03:58 +0900

    2021 07 12 003 1ban class commit 015

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name='create') #class에서 함수를 뱉어주는 mothod = as_view
 
 ]
 
