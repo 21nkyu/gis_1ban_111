3b429bc1ade74bb33f72b19e1ce147dc97692eaf
21nkyu <adele7178@naver.com>
2021-07-14 09:36:42 +0900

    2021 07 14 002 1ban class commit 018

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -7,6 +8,10 @@ app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create') #class에서 함수를 뱉어주는 mothod = as_view
 
--- ik_1ban_1/settings.py
+++ ik_1ban_1/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env_list = dict()
@@ -144,3 +146,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+# 어카운트앱 로그인 라우트로 가라
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+# 로그인이 성공했으면 어디로 갈지
