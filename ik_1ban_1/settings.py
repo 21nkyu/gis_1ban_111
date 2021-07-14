7204a1b3f9955c1022051763449378c1fe7d6ce6
21nkyu <adele7178@naver.com>
2021-07-14 10:45:51 +0900

    2021 07 14 003 1ban class commit 019

--- ik_1ban_1/settings.py
+++ ik_1ban_1/settings.py
@@ -53,7 +53,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accountapp'
+    'bootstrap4',
+    'accountapp',
+
 ]
 
 MIDDLEWARE = [
