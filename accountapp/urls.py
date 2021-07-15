b35dd8bb018af7ac65628fc0e337ec8d480b75ee
21nkyu <adele7178@naver.com>
2021-07-15 16:14:39 +0900

    2021 07 15 003 1ban class commit 023

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
 
 app_name = 'accountapp'
 
@@ -15,7 +15,9 @@ urlpatterns = [
 
     path('create/', AccountCreateView.as_view(), name='create'), #class에서 함수를 뱉어주는 mothod = as_view
 
-    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail')#detail로 갔을때 로직을 실행해주어라 (트리거), pk primary key 고유값
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),#detail로 갔을때 로직을 실행해주어라 (트리거), pk primary key 고유값
+
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
 
 ]
 
--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from accountapp.models import HelloWorld
 
@@ -50,5 +50,12 @@ class AccountCreateView(CreateView):        #createview 상속
 
 class AccountDetailView(DetailView):
     model = User
-    context_object_name = 'target_user'
+    context_object_name = 'target_user' #????
     template_name = 'accountapp/detail.html'#페이지 가 있어야한다 routing - urls.py
+
+class AccountUpdateView(UpdateView):
+    model = User                    #무엇을 업데이트 할 것인지
+    form_class = UserCreationForm   #수정할 내용을 넣어준다. 따로 접근하지 않아도 됨
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:hello_world')#일단은 헬로우월드
+    template_name = 'accountapp/update.html'
