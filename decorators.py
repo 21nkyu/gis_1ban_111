289111fe83f98999bfbbd4fcb69e5f397446dcfa
21nkyu <adele7178@naver.com>
2021-07-21 10:16:38 +0900

    2021 07 21 002 1ban class commit 028

---
+++ decorators.py
@@ -0,0 +1,60 @@
+
+#
+# def decorator(func):
+#     def decorated(input_text):
+#         print('함수 시작!')
+#         func(input_text)
+#         print('함수 끝!')
+#     return decorated
+#
+#
+# @decorator
+# def hello_world(input_text):
+#     print(input_text)
+#
+#
+#
+# hello_world('Hello_World!')
+# #
+# width
+# hight
+
+# def decorator(func):
+#     def decorated(width, height):
+#         if width > 0 and height > 0:
+#             func(width, height)
+#         else:
+#             print('Error')
+#     return decorated
+#
+#
+# @decorator
+# def area(width, height):
+#     print((width * height) * 1/2)
+#
+#
+#
+# area(4, 5)
+#
+
+def check_integer(func):
+    def decorated(width, height):
+        if width >= 0 and height >= 0:
+            return func(width, height)
+        else:
+            raise ValueError('input must be positive value')
+    return decorated
+
+@check_integer
+def rect_area(width, height):
+    return width * height
+
+rect_area(5, 6)
+
+# class User:
+#     def __init__(self, auth):
+#         self.is_autenticated = auth
+#
+# user =User(auth=False)
+
+
