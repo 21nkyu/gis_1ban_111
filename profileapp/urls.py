01dfc9aea1ee521119cf5dce15fed294df1a3713
21nkyu <adele7178@naver.com>
2021-07-28 10:19:19 +0900

    2021 07 28 001~002 1ban class commit 039

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateView
+from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'profileapp'
 
 urlpatterns = [
-    path('create/', ProfileCreateView.as_view(), name='create')
+    path('create/', ProfileCreateView.as_view(), name='create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 
 ]
+
+
+#무엇을 본건지 확인할 것인지 update에 명시
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,12 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
+@method_decorator(login_required, 'get')  #@method_decorator(login_required(login_url=), 'get')
+@method_decorator(login_required, 'post')
 
 class ProfileCreateView(CreateView):
     model = Profile
@@ -17,3 +22,12 @@ class ProfileCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    context_object_name = 'target_profile'  #프로필이기때문에 이름을 이렇게 // 접근 할때 이 이름으로 접근한다
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
