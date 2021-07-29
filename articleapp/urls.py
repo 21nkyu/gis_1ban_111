637554fcaac549307be6f9d5a247f130389b2dbc
21nkyu <adele7178@naver.com>
2021-07-29 15:50:03 +0900

    2021 07 29 004 1ban class commit 044

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+app_name = 'articleapp'  #적어야 아티클 앱으로 갈때 역산한다.
+
 urlpatterns = [
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list')
 
 ]
+
+# path 첫번째 들어갈 주소 /, 로직 클래스베이스드 뷰 as뷰
