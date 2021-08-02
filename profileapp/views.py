a37d1fc302b3ee4754434f8e7d14f6872cbd2ba4
21nkyu <adele7178@naver.com>
2021-08-02 15:00:30 +0900

    2021 08 02 001 1ban class commit 046

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
 # Create your views here.
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -10,9 +8,9 @@ from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
-@method_decorator(login_required, 'get')  #@method_decorator(login_required(login_url=), 'get')
-@method_decorator(login_required, 'post')
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
@@ -20,20 +18,21 @@ class ProfileCreateView(CreateView):
     template_name = 'profileapp/create.html'
 
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk}) #target_profile의 user의 pk를 연결
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})  # target_profile의 user의 pk를 연결
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileCreationForm
-    context_object_name = 'target_profile'  #프로필이기때문에 이름을 이렇게 // 접근 할때 이 이름으로 접근한다
+    context_object_name = 'target_profile'  # 프로필이기때문에 이름을 이렇게 // 접근 할때 이 이름으로 접근한다
     # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
 
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})            #self.object =target_profile
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})  # self.object =target_profile
