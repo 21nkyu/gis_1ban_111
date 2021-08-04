bbd6f7f73a17b290c5f775ddaf2f0175dc71dd19
21nkyu <adele7178@naver.com>
2021-08-04 09:20:58 +0900

    2021 08 04 001 1ban class commit 050

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView, ArticleDetailView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView
 
 app_name = 'articleapp'  #적어야 아티클 앱으로 갈때 역산한다.
 
@@ -9,8 +9,9 @@ urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
-
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
 
 ]
 
 # path 첫번째 들어갈 주소 /, 로직 클래스베이스드 뷰 as뷰
+#템플릿 만들것
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
@@ -24,3 +24,17 @@ class ArticleDetailView(DetailView):
     #만드는 것도 업고 바로 템플릿
     template_name = 'articleapp/detail.html'
     #이제 path 설정 html만들기기
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    # 어떤값 설정 할건지 form을 받는다
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    #수정했을때 향하는 페이지
+    # success_url = reverse_lazy('') #계시글 상세페이지로 //특정 계시글로 가기 위해서는 클래스 변수가 아니라 클래스를 하나 만들어야한다
+    #템플릿 어떤것을 사용할 것이냐
+    template_name = 'articleapp/update.html'
+    #routing을 해준다 urls
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk':self.object.pk})
+    # kwargs는 딕셔너리
