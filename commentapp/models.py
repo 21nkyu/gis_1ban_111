a8f93d07bfa8610b74f5cc6887fedc5c486dfe75
21nkyu <adele7178@naver.com>
2021-08-05 16:15:45 +0900

    2021 08 05 005 1ban class commit 058

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL,
+                                related_name='comment', null=True)
+
+    #작성한사람연결 일대일 연결이 아니다 연결하는건 유저
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='comment', null=True)
+
+    #content  작문의 데이터 텍스트 필드 뭘 넣어야 만든다
+    content = models.TextField(null=False)
+
+    #auto 자동으로 생성해줌
+    created_at = models.DateTimeField(auto_now_add=True)
