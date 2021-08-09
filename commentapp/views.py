0500306188019a0307302030b558495f36a4b901
21nkyu <adele7178@naver.com>
2021-08-09 15:15:10 +0900

    2021 08 09 003 1ban class commit 061

--- commentapp/views.py
+++ commentapp/views.py
@@ -13,5 +13,10 @@ class CommentCreateView(CreateView):
     form_class = CommentCreationForm
     template_name = 'commentapp/create.html'
 
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        form.instance.article_id = self.request.POST.get('article_pk')  #연결할 내용이 없다 어떤 개시글에 연결 됐느지 알수 없다 템플릿에서 정보를 넘겨줘야한다 comment create에 추가한 것을 가지고옴
+        return super().form_valid(form)
+
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
