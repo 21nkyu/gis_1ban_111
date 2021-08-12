700a6aa70bc818446ca529046375cfd4b67c8706
21nkyu <adele7178@naver.com>
2021-08-12 20:10:24 +0900

    2021 08 12 006 1ban class commit 071 fix st

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView
+from projectapp.views import ProjectCreateView, ProjectDetailView
 
 app_name = 'projectapp' #reverse(projcetapp 에 사용하기 위해서
 
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
 
 ]
--- projectapp/views.py
+++ projectapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
@@ -13,3 +13,9 @@ class ProjectCreateView(CreateView):
     form_class = ProjectCreationForm
     success_url = reverse_lazy('articleapp:list')#디테일뷰가 없으니 임시로
     template_name = 'projectapp/create.html' #에서 시각화 할거다
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    context_object_name = 'target_project'
+    template_name = 'projectapp/detail.html'
