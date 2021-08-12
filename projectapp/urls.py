4372bc4d152dcacd9a79d85557e381e368c2e454
21nkyu <adele7178@naver.com>
2021-08-12 15:54:54 +0900

    2021 08 12 006 1ban class commit 071

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,4 +1,10 @@
+from django.urls import path
+
+from projectapp.views import ProjectCreateView
+
+app_name = 'projectapp' #reverse(projcetapp 에 사용하기 위해서
 
 urlpatterns = [
+    path('create/', ProjectCreateView.as_view(), name='create'),
 
 ]
--- projectapp/views.py
+++ projectapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from projectapp.forms import ProjectCreationForm
+from projectapp.models import Project
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectCreationForm
+    success_url = reverse_lazy('articleapp:list')#디테일뷰가 없으니 임시로
+    template_name = 'projectapp/create.html' #에서 시각화 할거다
