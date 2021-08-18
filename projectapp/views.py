96839f209632c7f9b1d7b6ad956de400f75fc0a4
21nkyu <adele7178@naver.com>
2021-08-18 09:15:32 +0900

    2021 08 18 001 1ban class commit 072

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProjectCreateView(CreateView):
     model = Project
     form_class = ProjectCreationForm
     success_url = reverse_lazy('articleapp:list')#디테일뷰가 없으니 임시로
     template_name = 'projectapp/create.html' #에서 시각화 할거다
+
+    def get_success_url(self):
+        return reverse('projectapp:detail', kwargs={'pk': self.object.pk}) #완성되면 가는 화면
 
 
 class ProjectDetailView(DetailView):
