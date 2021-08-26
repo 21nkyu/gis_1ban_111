6a160332c32a3fbf9c0b0682c3d8f72b28b78244
21nkyu <adele7178@naver.com>
2021-08-26 16:15:37 +0900

    2021 08 26 004 1ban class commit 092

--- articleapp/models.py
+++ articleapp/models.py
@@ -10,7 +10,7 @@ class Article(models.Model):
                                related_name='article', null=True)
 
     project = models.ForeignKey(Project, on_delete=models.SET_NULL,
-                                related_name='article',null=True)
+                                related_name='article', null=True, blank=True)
 
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True) #프로필 미이더만들었음 //게시글 관련사진은 아티클
@@ -19,3 +19,6 @@ class Article(models.Model):
     created_at = models.DateField(auto_now_add=True, null=True)  #언제 작성된것인지 db자체적으로 저장될때 자동으로 할당해줘라
 
     like = models.IntegerField(default=0) #모델의 안의 인티저 필드를 사용할것이다. 처음은 0
+
+    # null db내에서 널이 가능하다
+    # 입력도 안받으려며는 프로젝트를 선택적으로 넣을수 있도록 blank=True
