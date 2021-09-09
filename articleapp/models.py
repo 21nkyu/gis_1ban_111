75e74e03598a2a09f87431b9f0f57df25c9872e7
21nkyu <adele7178@naver.com>
2021-09-09 16:28:30 +0900

    2021 09 09 001

--- articleapp/models.py
+++ articleapp/models.py
@@ -6,15 +6,14 @@ from projectapp.models import Project
 
 
 class Article(models.Model):
-    class Meta:
-        ordering = ['-pk']
+
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
                                related_name='article', null=True)
 
     project = models.ForeignKey(Project, on_delete=models.SET_NULL,
                                 related_name='article', null=True, blank=True)
 
-    title = models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=20, null=True)
     image = models.ImageField(upload_to='article/', null=True) #프로필 미이더만들었음 //게시글 관련사진은 아티클
     content = models.TextField(null=True)  #testfield 긴텍스트
 
@@ -24,3 +23,6 @@ class Article(models.Model):
 
     # null db내에서 널이 가능하다
     # 입력도 안받으려며는 프로젝트를 선택적으로 넣을수 있도록 blank=True
+
+    class Meta:
+        ordering = ['-pk']
